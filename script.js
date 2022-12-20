4b883dfc8207bcef2bf84ba27bc69c6261786540
Raphael <okolonnaemeka2017@gmail.com>
2022-12-20 12:08:23 -0500

    Update the main page so that the recent work section is created dynamically

--- script.js
+++ script.js
@@ -7,3 +7,65 @@ function closeMenu() {
   document.getElementById('mobile-menu').style.display = 'none';
 }
 closeMenu();
+
+
+const recentWorks = [
+    {
+        name: "Multi-Post Stories Gain+Glory",
+        skillsDisplay: ['Ruby on rails','CSS','JavScript','html'],
+        submitButton: 'See Project',
+        upCardimg: "#!"
+    },
+
+    {
+        name: "Multi-Post Stories Gain+Glory",
+        skillsDisplay: ['Ruby on rails','CSS','JavScript','html'],
+        submitButton: 'See Project',
+        upCardimg: "#!"
+    },
+
+    {
+        name: "Multi-Post Stories Gain+Glory",
+        skillsDisplay: ['Ruby on rails','CSS','JavScript','html'],
+        submitButton: 'See Project',
+        upCardimg: "#!"
+    },
+
+    {
+        name: "Multi-Post Stories Gain+Glory",
+        skillsDisplay: ['Ruby on rails','CSS','JavScript','html'],
+        submitButton: 'See Project',
+        upCardimg: "#!"
+    },
+
+    {
+        name: "Multi-Post Stories Gain+Glory",
+        skillsDisplay: ['Ruby on rails','CSS','JavScript','html'],
+        submitButton: 'See Project',
+        upCardimg: "#!"
+    },
+
+    {
+        name: "Multi-Post Stories Gain+Glory",
+        skillsDisplay: ['Ruby on rails','CSS','JavScript','html'],
+        submitButton: 'See Project',
+        upCardimg: "#!"
+    }
+]
+
+for (let i = 0; i < recentWorks.length; i += 1){
+    let tech = '';
+  
+
+  for (let j = 0; j < recentWorks[i].skillsDisplay.length; j += 1) {
+    tech += `<li >${recentWorks[i].skillsDisplay[j]}</li>`;
+  }
+  document.querySelector('.cards-container').innerHTML += `<div class="card-item">
+  <div class="up-card"></div>
+  <div class="down-card">
+      <h3>${recentWorks[i].name}</h3>
+      <ul class="skills">${tech}</ul>
+      <div class="button"><p>${recentWorks[i].submitButton}</p></div>
+  </div>    
+</div>`
+}
